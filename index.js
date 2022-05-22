3de98c392caa685e2a1c119ca52cdfa311cd9730
Rajesh-107 <applerb326@gmail.com>
2022-05-23 01:17:52 +0600

    connect with mongodb

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4tlkf.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const bikePartCollection = client.db('bike-manufacturer').collection('bikeparts');
+
+        app.get('/bikepart', async(req, res) => {
+            const query = {};
+            const cursor = bikePartCollection.find(query);
+            const bikeParts = await cursor.toArray();
+            res.send(bikeParts);
+        })
+
+    } finally {
+
+    }
+
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Daves Bike parts');
+});
+
+app.listen(port, () => {
+    console.log(`Bikeparts manufacturer  ${port}`);
+})
