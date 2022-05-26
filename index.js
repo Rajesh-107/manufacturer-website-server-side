291c0556667de287b706be6b041abfaf154c29b2
Rajesh-107 <applerb326@gmail.com>
2022-05-26 12:33:25 +0600

    verify admin middleware

--- index.js
+++ index.js
@@ -37,6 +37,17 @@ async function run() {
         const bikePartCollection = client.db('bike-manufacturer').collection('bikeparts');
         const bookingCollection = client.db('bike-manufacturer').collection('booking');
         const userCollection = client.db('bike-manufacturer').collection('users');
+        const productCollection = client.db('bike-manufacturer').collection('product');
+
+        const verifyAdmin = async(req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester })
+            if (requesterAccount.role === 'admin') {
+                next()
+            } else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+        }
 
         app.get('/bikepart', async(req, res) => {
             const query = {};
@@ -45,12 +56,12 @@ async function run() {
             res.send(bikeParts);
         })
 
-        app.get('/user', verifyJWT, async(req, res) => {
+        app.get('/user', async(req, res) => {
             const users = await userCollection.find().toArray();
             res.send(users);
         })
 
-        app.get('/admin/:email', async(req, res) => {
+        app.get('/admin/:email', verifyJWT, async(req, res) => {
             const email = req.params.email;
             const user = await userCollection.findOne({ email: email });
             const isAdmin = user.role === 'admin';
@@ -58,24 +69,19 @@ async function run() {
         })
 
 
-        app.put('/user/admin/:email', verifyJWT, async(req, res) => {
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async(req, res) => {
             const email = req.params.email;
-            const requester = req.decoded.email;
-            const requesterAccount = await userCollection.findOne({ email: requester })
-            if (requesterAccount.role === 'admin') {
-                const filter = { email: email };
-                const updateDoc = {
-                    $set: { role: 'admin' },
-                };
-                const result = await userCollection.updateOne(filter, updateDoc)
-                res.send(result);
-            } else {
-                res.status(403).send({ message: 'forbidden' });
-            }
+
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc)
+            res.send(result);
 
         })
 
-        app.put('/user/:email', async(req, res) => {
+        app.put('/user/:email', verifyJWT, async(req, res) => {
             const email = req.params.email;
             const user = req.body;
             const filter = { email: email };
@@ -102,7 +108,19 @@ async function run() {
 
         })
 
-        app.post('/booking', verifyJWT, async(req, res) => {
+        app.post('/bikepart', verifyJWT, async(req, res) => {
+            const newParts = req.body;
+            const result = await bikePartCollection.insertOne(newParts);
+            res.send(result);
+        })
+
+        app.post('/product', verifyJWT, verifyAdmin, async(req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            res.send(result);
+        })
+
+        app.post('/booking', async(req, res) => {
             const booking = req.body;
             const result = await bookingCollection.insertOne(booking);
             res.send(result);
